b9c2fe655b4ff365690d469bdd04cc6ba8772ca4
Veronishi <42645038+Veronishi@users.noreply.github.com>
2023-02-09 15:39:02 +0100

    create exam

--- app/src/main/java/com/example/examcountdown/MainActivity.kt
+++ app/src/main/java/com/example/examcountdown/MainActivity.kt
@@ -22,9 +22,10 @@ class MainActivity : AppCompatActivity() {
         tabs.setupWithViewPager(viewPager)
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        fab.setOnClickListener { view ->
+        fab.setOnClickListener { /*view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                    .setAction("Action", null).show()*/
+            setContentView(R.layout.create_exam)
         }
     }
 }
