bd1d2680a53a7106382d3ea98ac7386460ffcbba
Veronishi <42645038+Veronishi@users.noreply.github.com>
2023-02-28 10:53:23 +0100

    opens main activity when notification tapped

--- app/src/main/java/com/example/examcountdown/Notification.kt
+++ app/src/main/java/com/example/examcountdown/Notification.kt
@@ -1,6 +1,7 @@
 package com.example.examcountdown
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -15,10 +16,15 @@ class Notification : BroadcastReceiver()
 {
     override fun onReceive(context: Context, intent: Intent)
     {
+        val activityIntent = Intent(context, MainActivity::class.java)
+        val id = 0
+        val pendingIntent = PendingIntent.getActivity(context, id, activityIntent, PendingIntent.FLAG_CANCEL_CURRENT)
+
         val notification = NotificationCompat.Builder(context, channelID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle(intent.getStringExtra(titleExtra))
             .setContentText(intent.getStringExtra(messageExtra))
+            .setContentIntent(pendingIntent)
             .build()
 
         val  manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
